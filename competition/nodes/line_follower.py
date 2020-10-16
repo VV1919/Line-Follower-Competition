9e5461c2537bb3f46a52317e91149279f9fdaef5
rider <rider@riders.ai>
2020-10-16 17:53:18 +0000

    File_changes:_./competition/nodes/line_follower.py

--- competition/nodes/line_follower.py
+++ competition/nodes/line_follower.py
@@ -65,7 +65,7 @@ class LineFollower:
             # ENTER YOUR LINE DETECTING CODE HERE
             self.sensor_array_len = len(self.sensor_row)
             print(self.sensor_row)
-            line_value_array = 
+            line_value_array = []
             for i in range(self.sensor_array_len):
                 if(self.sensor_row[i] > 0):
                     line_value_array.append(self.sensor_row[i])
